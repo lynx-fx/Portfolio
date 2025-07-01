84205e5a6262341172a03b3fdfb6d9a0fd696f64
lynx-fx <anupbhujel07@gmail.com>
2025-07-01 20:52:22 +0545

    Cleaning the remaints

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-import "./styles.css"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
-import TerminalSection from "./components/TerminalSection"
-import SkillsSection from "./components/SkillsSection"
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <main className="main-content">
-        <TerminalSection />
-        <section className="about-preview">
-          <h2>About Me</h2>
-          <p>
-            I'm a passionate web developer and cybersecurity enthusiast with a focus on building secure, efficient web
-            applications. My expertise spans front-end development, back-end systems, and security implementation.
-          </p>
-          <button className="btn">Learn More</button>
-        </section>
-        <SkillsSection />
-      </main>
-      <Footer />
+    <div>
+      
     </div>
   )
 }
-
-export default App
